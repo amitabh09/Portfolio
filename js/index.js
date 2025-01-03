c2beed5a3f1586ba3f7c33700f3a34a85983b779
Amitabh Shahi <amitabhshahi09@gmail.com>
2025-01-04 01:04:32 +0530

    fixed section 1 and 2 with mobile responsiveness

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 function displayHeading(title) {
     return `<hr class="hrHalf"><h1>${title}</h1><hr class="hrHalf"></hr>`;
 }
-// ***************************************************************************************************************************************//
+ 
 
 // ***************************************************** Header **************************************************************************//
 const navArray = [
@@ -26,32 +26,30 @@ function displayNavbar(arr) {
     return str;
 }
 
-document.getElementsByTagName('header')[0].innerHTML = displayNavbar(navArray);
-// ***************************************************************************************************************************************//
+// document.getElementsByTagName('header')[0].innerHTML = displayNavbar(navArray);
+ 
 
 // ************************************************* Section 1 ***************************************************************************//
-const introArray = ['Hello, I am','Amitabh Shahi',`I'm an Web Developer who specializes in creating unique & appealing websites as well as web applications using the latest technologies and developing firmware for interactive IOT devices. Currently, I am experienced in both Web Development and IOT.`];
+// const introArray = ['Hello, I am','Amitabh Shahi',`I'm an Web Developer who specializes in creating unique & appealing websites as well as web applications using the latest technologies and developing firmware for interactive IOT devices. Currently, I am experienced in both Web Development and IOT.`];
 
-function displaySec1(arr) {
-    let str = `<p>${arr[0]}</p><h1>${arr[1]}</h1><p>${arr[2]}</p>`;
-    str += `<br><br><button class="head2button">Check Out My Projects!</button>`;
-    return str;
-}
+// function displaySec1(arr) {
+//     let str = `<p>${arr[0]}</p><h1>${arr[1]}</h1><p>${arr[2]}</p>`;
+//     str += `<br><br><button class="head2button">Check Out My Projects!</button>`;
+//     return str;
+// }
 
-document.getElementsByClassName('sec1')[0].innerHTML = displaySec1(introArray);
-// ***************************************************************************************************************************************//
+// document.getElementsByClassName('sec1')[0].innerHTML = displaySec1(introArray);
+
 
 // ************************************************* Section 2 ***************************************************************************//
 
-let aboutText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi aliquam dolore temporibus perspiciatis ipsam consectetur voluptatum velit, optio commodi deserunt deleniti quisquam incidunt ea quae, labore facere sed obcaecati accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, nesciunt! Vel ab quia error maiores! Esse enim beatae quae repellendus odio perferendis magnam officiis id. Consectetur temporibus deleniti consequatur tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi aliquam dolore temporibus perspiciatis ipsam consectetur voluptatum velit, optio commodi deserunt deleniti quisquam incidunt ea quae, labore facere sed obcaecati accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, nesciunt! Vel ab quia error maiores! Esse enim beatae quae repellendus odio perferendis magnam officiis id. Consectetur temporibus deleniti consequatur tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit.`;
 
 document.getElementsByClassName('sec2Heading')[0].innerHTML = displayHeading('About Me');
-document.getElementsByClassName('sec2Part1')[0].innerHTML = aboutText;
-// ***************************************************************************************************************************************//
+
 
 // ************************************************* Section 3 ***************************************************************************//
 document.getElementsByClassName('sec3Heading')[0].innerHTML = displayHeading('Skills');
-// ***************************************************************************************************************************************//
+
 
 // ************************************************* Section 4 ***************************************************************************//
-// ***************************************************************************************************************************************//
+ 
